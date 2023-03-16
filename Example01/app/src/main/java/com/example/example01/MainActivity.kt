96dace73193a0809029b2d1e0c225d1eca8f909c
Dan920-Dev <00138218@uca.edu.sv>
2023-03-15 21:02:52 -0600

    Add all views and base code

--- Example01/app/src/main/java/com/example/example01/MainActivity.kt
+++ Example01/app/src/main/java/com/example/example01/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.example01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+
+    // Variables a usar
+    private lateinit var email : EditText
+    private lateinit var message : EditText
+    private lateinit  var send : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bind()
+
+        // Action
+        send.setOnClickListener { _ ->
+            val emailUser  = email.text
+            val messageUser = message.text
+        }
+
     }
+
+
+    fun bind(){
+        email = findViewById(R.id.email_edit_text)
+        message = findViewById(R.id.message_edit_text)
+        send = findViewById(R.id.action_send)
+    }
+
 }
