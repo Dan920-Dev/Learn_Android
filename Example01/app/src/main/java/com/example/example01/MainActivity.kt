3964aa49454e4ef510f7cb7b4b4b56b88c916e98
Dan920-Dev <00138218@uca.edu.sv>
2023-03-16 11:32:50 -0600

    Add validation

--- Example01/app/src/main/java/com/example/example01/MainActivity.kt
+++ Example01/app/src/main/java/com/example/example01/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.example01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,9 +10,9 @@ import android.widget.EditText
 class MainActivity : AppCompatActivity() {
 
     // Variables a usar
-    private lateinit var email : EditText
-    private lateinit var message : EditText
-    private lateinit  var send : Button
+    private lateinit var email: EditText
+    private lateinit var message: EditText
+    private lateinit var send: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +21,44 @@ class MainActivity : AppCompatActivity() {
 
         // Action
         send.setOnClickListener { _ ->
-            val emailUser  = email.text
+
+            val emailUser = email.text
             val messageUser = message.text
+
+
+            // Validation
+            if (emailUser.isEmpty() && messageUser.isNotEmpty()) {
+
+                // Show ERROR
+                val errorEmail = R.string.errorEmail
+                email.hint = getString(errorEmail)
+
+            } else if (messageUser.isEmpty() && emailUser.isNotEmpty()) {
+
+                // Show Error
+                val errorMessage = R.string.errorMessage
+                message.hint = getString(errorMessage)
+
+            } else if (emailUser.isEmpty() && messageUser.isEmpty()) {
+
+                // ERROR, email and message is empty
+                val errorEmail = R.string.errorEmail
+                email.hint = getString(errorEmail)
+
+                val errorMessage = R.string.errorMessage
+                message.hint = getString(errorMessage)
+
+            } else {
+
+                // everything's fine!, open new activity
+                var intent = Intent(this, MessageActivity::class.java)
+                startActivity(intent)
+            }
         }
 
     }
 
-
-    fun bind(){
+    fun bind() {
         email = findViewById(R.id.email_edit_text)
         message = findViewById(R.id.message_edit_text)
         send = findViewById(R.id.action_send)
