ded3f60cf0fe659330a97c504c03746bc326bc11
Dan920-Dev <00138218@uca.edu.sv>
2023-05-18 15:40:53 -0600

    initial commit of cars collection

---
+++ CarsCollection/app/src/main/java/com/jder00138218/carscollection/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jder00138218.carscollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
